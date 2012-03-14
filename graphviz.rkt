99844557d911731f9ecff7030f1a97df96473c43
Stephen Austin <antirealist@gmail.com>
2012-03-13 22:37:40 -0400

    the graphviz utils

---
+++ graphviz.rkt
@@ -0,0 +1,38 @@
+#lang racket
+
+;;; Racket translation of Graphviz utils from Land Of Lisp
+;;; Steve Austin 
+;;; March 2012
+
+(require srfi/13)   ;; For string-map
+
+(define *wizard-nodes* '((living-room (you are in the living room. 
+                                           a wizard is snoring loudly on the couch.))
+                         (garden (you are in a beautiful garden. 
+                                      there is a well in front of you.))
+                         (attic (you are in the attic. 
+                                     there is a giant welding torch in the corner.))))
+
+(define *wizard-edges* '((living-room (garden west door)
+                                      (attic upstairs ladder))                             
+                         (garden (living-room east door))
+                         (attic (living-room downstairs ladder))))
+
+
+(define (char-alphanumeric? chr)
+  (or (char-alphabetic? chr) (char-numeric? chr))) 
+
+(define (substitute-if chr a-test? str)
+  (define (sub-test any-c)
+    (if (a-test? any-c) chr
+        any-c))
+  (string-map sub-test str))
+
+;; this version of dot-name will not convert '24 to "24"
+;; racket does not recognize '24 as a valid scheme symbol
+
+(define (dot-name exp)
+  (substitute-if #\_ (lambda (c) (not (char-alphanumeric? c))) (symbol->string exp)))
+
+
+
